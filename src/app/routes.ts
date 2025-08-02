1e222c0b5b69bac57f6d396f12467b2781b88e1c
suronjit <suronjit797@gmail.com>
2025-08-02 11:52:49 +0600

    remove unusal packages

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -20,37 +20,6 @@ moduleRoute.forEach((route) =>
 
 export default router;
 
-//
-//
-//
-//! old from other projects
-//
-//
-//
-
-// import express from "express";
-// import userRouter from "./user/user.routes";
-// import { uploadCloudinary } from "../utils/uploadToCloudinary";
-// import paymentRouter from "./payments/payment.routes";
-// import paymentHistoryRouter from "./paymentHistory/paymentHistory.routes";
-// // import bukkuAxios from "../utils/axiosUtils";
-
-// const router = express.Router();
-
-// // const testRouter = async (req, res, next) => {
-// //   const data = await bukkuAxios.get("/sales/payments");
-// //   console.log(data);
-// //   res.send({ res: "test ", data: data.data });
-// // };
-
-// const moduleRoute = [
-//   { path: "/users", routes: userRouter },
-//   { path: "/payments", routes: paymentRouter },
-//   { path: "/payments-history", routes: paymentHistoryRouter },
-//   // { path: "/test", routes: testRouter },
-// ];
-
-// moduleRoute.forEach((route) => router.use(route.path, route.routes));
 
 // // image upload
 // router.post("/upload", uploadCloudinary.single("photo"), (req, res, next) => {
